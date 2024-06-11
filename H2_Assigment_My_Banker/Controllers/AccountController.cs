6e10453fd06278c769b878fee893935c1c973bc9
janick004 <78735480+janick004@users.noreply.github.com>
2024-06-11 19:58:05 +0200

    s

--- H2_Assigment_My_Banker/Controllers/AccountController.cs
+++ H2_Assigment_My_Banker/Controllers/AccountController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using H2_Assigment_My_Banker.Models;
+using H2_Assigment_My_Banker.Models.Cards;
 
 namespace H2_Assigment_My_Banker.Controllers
 {
     internal class AccountController
     {
+        private const string PREFIX = "3520";
+
+        public BankAccount SetupBankAccount(Bank bank)
+        {
+            RandomGenerator RG = new RandomGenerator();
+            return new BankAccount(bank, 0, PREFIX + RG.GenerateNumbers(14));
+        }
     }
 }
-s
