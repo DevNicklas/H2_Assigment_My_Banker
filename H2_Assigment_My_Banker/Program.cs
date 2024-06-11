1aebe977b2549ea64e564b092b17c9303ab3883e
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2024-06-11 12:52:47 +0200

    Add project files.

---
+++ H2_Assigment_My_Banker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Assigment_My_Banker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
